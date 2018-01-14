e63c07d493fc816ebe4bafe85181ad53a95bd8d8
joylty94 <wowmen1757@gmail.com>
2018-01-14 16:30:09 +0900

    사진첩 미완

---
+++ fds-PR/index.js
@@ -0,0 +1,15 @@
+const page = document.querySelector('.page')
+const page2 = document.querySelector('.page2')
+const front = document.querySelector('.front')
+const back = document.querySelector('.back')
+const fClick = document.querySelector('.front-click')
+const bClick = document.querySelector('.back-click')
+const home = document.querySelector('.front-padding2')
+fClick.addEventListener('click', e => {
+  page.classList.add('active')
+  fClick.classList.add('readable-hidden')
+})
+bClick.addEventListener('click', e => {
+  page.classList.remove('active')
+  fClick.classList.remove('readable-hidden')
+})
