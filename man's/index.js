594c1fa7ad5a350ba14357e6cdfce4e690c0d428
joylty94 <wowmen1757@gmail.com>
2018-01-15 23:08:02 +0900

    man

--- man's/index.js
+++ man's/index.js
@@ -8,14 +8,11 @@ const auth = firebase.auth()
 const storage = firebase.storage()
 const database = firebase.database()
 const loginButton = document.querySelector('.login-button')
+const logoutButton = document.querySelector('.logout-button')
 const fileInputEl = document.querySelector('.file-input');
 const imgListEl = document.querySelector('.img-list');
 const backButton = document.querySelector('.back-button');
 const nextButton = document.querySelector('.next-button');
-
-fileInputEl.addEventListener('click', e => {
-  alert('구글 로그인해야 업로드 가능하닷!!!!')
-})
 
 loginButton.addEventListener('click', async e => {
   const result = await firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -23,24 +20,35 @@ loginButton.addEventListener('click', async e => {
     var user = result.user;
     console.log(user)
   })
+  loginButton.classList.add('readable-hidden')
+  looutButton.classList.remove('readable-hidden')
   fileInputEl.addEventListener('change', async e => {
     const uid = auth.currentUser.uid;
     console.log(fileInputEl.files[0])
     const refStr = `/img/${auth.currentUser.uid}:${new Date().getTime()}`;
     const snapshot = await storage.ref(refStr).put(fileInputEl.files[0]);
 
-    database.ref(`users/${uid}/img`).push({
+    database.ref(`/img`).push({
       downloadURL: snapshot.downloadURL,
       fileName: fileInputEl.files[0].name
     })
   })
   refreshImges()
+  logoutButton.addEventListener('click', async e => {
+    e.stopPropagation();
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    })
+    loginButton.classList.remove('readable-hidden')
+    logoutButton.classList.add('readable-hidden')
+    refreshWeather();
+  })
 })
 
 async function refreshImges() {
   const uid = auth.currentUser.uid
   const snapshot = await database
-    .ref(`users/${uid}/img`)
+    .ref(`/img`)
     .orderByKey()
     .limitToFirst(IMAGE_PER_PAGE + 1)
     .startAt(nextStackKey[nextStackKey.length - 1] || '') // nextKey가 undefind이면 ''열이 실행된다.(startAt('')은 그냥 실행된다.)
