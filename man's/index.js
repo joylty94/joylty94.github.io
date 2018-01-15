94ded4e40b0047a60fe1a2f05e88eddaf826d970
joylty94 <wowmen1757@gmail.com>
2018-01-15 22:23:31 +0900

    Man's

---
+++ man's/index.js
@@ -0,0 +1,86 @@
+const IMAGE_PER_PAGE = 1;
+
+let nextKey;
+let nextStackKey = [];
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth()
+const storage = firebase.storage()
+const database = firebase.database()
+const loginButton = document.querySelector('.login-button')
+const fileInputEl = document.querySelector('.file-input');
+const imgListEl = document.querySelector('.img-list');
+const backButton = document.querySelector('.back-button');
+const nextButton = document.querySelector('.next-button');
+
+fileInputEl.addEventListener('click', e => {
+  alert('구글 로그인해야 업로드 가능하닷!!!!')
+})
+
+loginButton.addEventListener('click', async e => {
+  const result = await firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    console.log(user)
+  })
+  fileInputEl.addEventListener('change', async e => {
+    const uid = auth.currentUser.uid;
+    console.log(fileInputEl.files[0])
+    const refStr = `/img/${auth.currentUser.uid}:${new Date().getTime()}`;
+    const snapshot = await storage.ref(refStr).put(fileInputEl.files[0]);
+
+    database.ref(`users/${uid}/img`).push({
+      downloadURL: snapshot.downloadURL,
+      fileName: fileInputEl.files[0].name
+    })
+  })
+  refreshImges()
+})
+
+async function refreshImges() {
+  const uid = auth.currentUser.uid
+  const snapshot = await database
+    .ref(`users/${uid}/img`)
+    .orderByKey()
+    .limitToFirst(IMAGE_PER_PAGE + 1)
+    .startAt(nextStackKey[nextStackKey.length - 1] || '') // nextKey가 undefind이면 ''열이 실행된다.(startAt('')은 그냥 실행된다.)
+    .once('value')
+  const img = snapshot.val()
+  const keys = Object.keys(img);
+  nextKey = keys[keys.length - 1];
+
+  imgListEl.innerHTML = '';
+
+  const imageArr = Object.entries(img).slice(0, IMAGE_PER_PAGE)
+  for (let [imgId, imgObj] of imageArr) {
+    const imageEl = document.createElement('img');
+    const fileNameEl = document.createElement('div')
+
+    const splitName = imgObj.fileName.split('.');
+    fileNameEl.textContent = splitName[0];
+
+    imageEl.src = imgObj.downloadURL;
+
+    imgListEl.appendChild(imageEl);
+    imgListEl.appendChild(fileNameEl);
+    imageEl.classList.add('img-list__item')
+    fileNameEl.classList.add('img-list__fileName')
+  }
+}
+
+nextButton.addEventListener('click', async e => {
+  nextStackKey.push(nextKey)
+  refreshImges()
+})
+
+backButton.addEventListener('click', async e => {
+  nextStackKey.pop()
+  refreshImges()
+})
+
+// 로그인 유지
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    refreshImges()
+  }
+});
